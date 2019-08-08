5dc627eca2676f11e52422aad546e7f68fae9989
Tobias Reich <tobias.reich.ich@gmail.com>
2019-08-08 13:53:41 +0200

    Grid template

---
+++ src/components/template_grids/template_grids.data.js
@@ -0,0 +1,9 @@
+module.exports = {
+	section_grid_3_3_3_3: require('../section_grid_3_3_3_3/section_grid_3_3_3_3.data'),
+	section_grid_4_4_4: require('../section_grid_4_4_4/section_grid_4_4_4.data'),
+	section_grid_4_8: require('../section_grid_4_8/section_grid_4_8.data'),
+	section_grid_6_6: require('../section_grid_6_6/section_grid_6_6.data'),
+	section_grid_8_4: require('../section_grid_8_4/section_grid_8_4.data'),
+	section_grid_12: require('../section_grid_12/section_grid_12.data'),
+	cookie: require('../cookie/cookie.data')
+}

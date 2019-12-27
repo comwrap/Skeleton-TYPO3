f91b48fd21d4dd6a9e454b4e9f7c75309e9a6a5c
Tobias Reich <tobias.reich.ich@gmail.com>
2019-12-27 15:45:00 +0100

    Teaser

--- src/components/teaser/teaser.data.js
+++ src/components/teaser/teaser.data.js
@@ -1,24 +1,10 @@
 module.exports = {
-	image: {
-		alternative: 'Lorem ipsum',
-		src: '#',
-		x: 0.5,
-		y: 0.5
-	},
+	image: require('../background_styles/background_styles.data'),
 	headline: {
-		text: 'Lorem ipsum',
-		type: 'h1',
-		style: 'h2',
-		position: 'left'
-	},
-	subheadline: {
-		text: 'Lorem ipsum',
-		type: 'h2',
-		style: 'h3',
-		position: 'left'
+		text: 'Lorem ipsum'
 	},
 	bodytext: '<p>Lorem ipsum</p>',
-	button: {
+	link: {
 		href: {
 			label: 'Label',
 			value: '#',
